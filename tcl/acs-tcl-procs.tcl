a25fcd265ec4cbaa80e305e9551d9433c48627fa
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-25 19:47:46 +0000

    Prefer '{}' to '[list]' when creating empty lists

--- tcl/acs-tcl-procs.tcl
+++ tcl/acs-tcl-procs.tcl
@@ -2,7 +2,7 @@ ad_proc -public -deprecated lmap {list proc_name} {
     Applies proc_name to each item of the list, appending the result of 
     each call to a new list that is the return value.
 } {
-    set lmap [list]
+    set lmap {}
     foreach item $list {
         lappend lmap [safe_eval $proc_name $item]
     }
